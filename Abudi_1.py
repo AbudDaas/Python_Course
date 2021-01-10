51e2f51e00ef01258aa6124967f17759508eddfc
Abudi <abdulrazakdaas0@gmail.com>
2021-01-10 21:05:11 +0300

    Add Strings Code

--- Abudi_1.py
+++ Abudi_1.py
@@ -1,37 +1,40 @@
-x = 1
-n = 2.8
-c = ("3")
-
-
-z = int(1)
-y = int(2.8)
-q = int("3")
-print(z)
-print(y)
-print(q)
-
-
-
-a =str(1)
-b =str(2.2)
-d =str('3')
-
-print(type(x))
-print(type(n))
-print(type(c))
-
-
-a ="Hello world"
-b ="gunaydın"
-print(a)
-print(b)
-
-a = """Lorem ipsum dolor sit amet,
-consectetur adipiscing elit,
-sed do eiusmod tempor incididunt
-ut labore et dolore magna aliqua."""
-print(len(a))
-a = "Hello, World!"
-print(a[0])
-a = "Hello, World!"
-print(len(a))
+#=========================================================================================================================#
+#========================================================>Python Booleans<================================================#
+#=========================================================================================================================#
+print(9 < 10)      #========>>true
+print(9 == 10)     #========>>false
+print(9 > 10)      #========>>false
+#======================================================
+A = 50
+Z = 30
+if A < Z :
+    print("Z is greater than A")    #==========>"A is greater than Z"
+else:
+    print("A is greater than Z")
+#=======================================================
+x = "Hello"
+y = 15
+print(bool(y))     #===========>true
+print(bool(x))     #===========>true
+#-=================================>>Etiketlerden birini koyarsanız,yanlış çıkacaktır.<<=======================================#
+print(bool(False))   #========>>false
+print(bool(None))    #========>>false
+print(bool(0))       #========>>false
+print(bool(""))      #========>>false
+print(bool(()))      #========>>false
+print(bool([]))      #========>>false
+print(bool({}))      #========>>false
+#======================================================
+def myFunction() :
+  return True           #========>True
+print(myFunction())
+#=====================================================
+def myFunction() :
+  return True
+if myFunction():
+  print("YES!") #======>YES
+else:
+  print("NO!")
+#======================================================
+x = 200 
+print(isinstance(x,(int))     #===========>>true
