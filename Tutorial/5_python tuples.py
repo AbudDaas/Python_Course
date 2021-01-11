c4da9e9194157e7d93a8e592a4d5310f95d0b50a
Abudi <abdulrazakdaas0@gmail.com>
2021-01-11 18:37:20 +0300

    new add strings code

---
+++ Tutorial/5_python tuples.py
@@ -0,0 +1,11 @@
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+#=====================================
+thistuple = ("apple",)
+print(type(thistuple))
+thistuple = ("apple")
+print(type(thistuple))
+#================================
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+#===================================
