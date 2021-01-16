00ab514210a850397911856a4c7290facffeed93
Abudi <abdulrazakdaas0@gmail.com>
2021-01-16 13:46:31 +0300

    newadd strings code

--- Tutorial/5_python tuples.py
+++ Tutorial/5_python tuples.py
@@ -9,3 +9,40 @@ print(type(thistuple))
 mytuple = ("apple", "banana", "cherry")
 print(type(mytuple))
 #===================================
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+#================================
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple)
+#============================
+fruits = ("apple", "banana", "cherry")
+(green, yellow, red) = fruits
+print(green)
+print(yellow)
+print(red)
+#=============================
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+(green, yellow, *red) = fruits
+print(green)
+print(yellow)
+print(red)
+#==============================
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+(green, *tropic, red) = fruits
+print(green)
+print(tropic)
+print(red)
+#=============================
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+#==========================
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1
+#==============================
